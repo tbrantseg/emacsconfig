dfabe206275a38d1ce31bc3d41ed54682763215e
Tom Brantseg <tbrantse@iastate.edu>
2016-06-02 12:05:58 -0500

    Org-babel support for dot, ditaa, python, gnuplot, plantuml

--- tb-org-setup.el
+++ tb-org-setup.el
@@ -12,25 +12,15 @@
   (and (buffer-file-name)
        (file-exists-p (buffer-file-name))
        (reftex-parse-all))
-  (define-key org-mode-map (kbd "C-c (") 'reftex-citation)
-  ;; (setq reftex-cite-format
-  ;;     '(
-  ;;       (?\C-m . "\\cite[]{%l}")
-  ;;       (?t . "\\textcite{%l}")
-  ;;       (?a . "\\autocite[]{%l}")
-  ;;       (?p . "\\parencite{%l}")
-  ;;       (?f . "\\footcite[][]{%l}")
-  ;;       (?F . "\\fullcite[]{%l}")
-  ;;       (?x . "%l")
-  ;;       (?X . "{%l}")
-  ;;       )))
-  )
+  (define-key org-mode-map (kbd "C-c (") 'reftex-citation))
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (add-hook 'org-mode-hook 'org-mode-reftex-setup)
 (setq ebib-preload-bib-files
       (list "~/Library/texmf/bibtex/bib/biblio.bib"))
 (org-add-link-type "ebib" 'ebib)
+
+;; Set up org latex export
 (setq org-latex-pdf-process (list "latexmk -f -pdf %f"))
 (setq org-latex-with-hyperref nil)
 (add-to-list 'org-latex-classes
@@ -52,6 +42,24 @@
   (require 'org-ref)
   (visual-line-mode))
 (add-hook 'org-mode-hook 'org-ref-setup)
+
+;; Org-babel setup
+;; Dot, ditaa, python, gnuplot, plantuml
+(defun my-babel-languages ()
+  (add-to-list 'org-src-lang-modes '("dot" . graphviz-dot))
+  (org-babel-do-load-languages   
+   'org-babel-load-languages
+   '((dot . t)
+     (ditaa . t)
+     (python . t)
+     (gnuplot . t)
+     (plantuml . t)
+     (python . t))))
+(add-hook 'org-mode-hook 'my-babel-languages)
+
+(defun my-org-confirm-babel-evaluate (lang body)
+  (not (string= lang "dot")))
+(setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
 
 ;; Setup org agenda
 (setq org-agenda-files (list "~/org/work.org" "~/org/home.org"))
