27e6978ddcd4634c730eb340948a18770ddf5e4e
Tom Brantseg <11296858+tbrantseg@users.noreply.github.com>
2018-07-12 11:56:19 -0500

    Update emacs-init.el

--- emacs-init.el
+++ emacs-init.el
@@ -1,10 +1,13 @@
 ;; emacs-init.el
 ;; This file replaces itself with the contents of emacs-init.org when first run.
 
-(require 'org)
-(find-file "~/emacs-init/emacs-init.org")
-(org-babel-tangle)
-(load-file "~/emacs-init/emacs-init.el")
-(byte-compile-file "~/emacs-init/emacs-init.el")
+(let* ((install-dir (concat user-emacs-directory "emacsconfig/"))
+       (org-file (concat install-dir "emacs-init.org"))
+       (el-file (concat install-dir "emacs-init.el")))
+      (require 'org)
+      (find-file org-file)
+      (org-babel-tangle)
+      (load-file el-file)
+      (byte-compile-file el-file))
 
 ;; emacs-init.el ends here.
